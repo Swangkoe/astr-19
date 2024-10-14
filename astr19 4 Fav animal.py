062e6008ca7fda3ebdcb54f37e3ed4d9fa7a2364
Samuel Wang Koenigsberg <swangkoe@ucsc.edu>
2024-10-14 02:04:35 -0700

    Add files via upload

---
+++ astr19 4 Fav animal.py
@@ -0,0 +1,9 @@
+class Sperm_Whale:
+    def _init_(self,arms,legs,eyes,tail,furry):
+        self.arms = 1.5
+        self.arms = 5
+        self.eyes = 2
+        self.tail = True
+        self.furry = False
+    
+        
